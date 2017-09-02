0dc921066560ed99bad87f290e160a61771313cc
Steffen Castle <steffen.castle@gmail.com>
2017-09-02 21:49:17 +0200

    Change wavlib to use librosa

--- speechless/wavlib.py
+++ speechless/wavlib.py
@@ -3,7 +3,7 @@ __author__ = 'Steffen'
 import requests
 import re
 import numpy as np
-from scikits.audiolab import wavread, wavwrite
+import librosa
 import os, random
 
 def download_from_tag(tag='field-recording', directory='environmental', num_files=100):
@@ -41,26 +41,22 @@ def download_files(urls, directory='wavfiles'):
             outfile.write(response.content)
 
 def mix_wavs_raw(foreground_wav, background_wav, foreground_volume=0.6, background_volume=0.4):
-    foreground_data, foreground_rate, fg_enc  = wavread(foreground_wav)
-    background_data, background_rate, bg_enc = wavread(background_wav)
-
-    # Make sure both are stereo
-    if len(foreground_data.shape) != 2:
-        foreground_data = np.repeat(foreground_data[:,np.newaxis], 2, axis=1)
-    if len(background_data.shape) != 2:
-        background_data = np.repeat(background_data[:,np.newaxis], 2, axis=1)
-
-    # Downsample both to 16 kHz
     desired_sample_rate = 16000
-    foreground_length = 1.0 * foreground_data.shape[0] / foreground_rate
-    background_length = 1.0 * background_data.shape[0] / background_rate
-    get_interp = lambda x, length: np.arange(0, length, 1.0 / x)
-    foreground_data = np.array([np.interp(get_interp(desired_sample_rate, foreground_length),
-                                get_interp(foreground_rate, foreground_length),
-                                foreground_data[:,i]) for i in range(2)]).transpose()
-    background_data = np.array([np.interp(get_interp(desired_sample_rate, background_length),
-                                get_interp(background_rate, background_length),
-                                background_data[:,i]) for i in range(2)]).transpose()
+
+    foreground_data, foreground_rate = librosa.load(foreground_wav, sr=desired_sample_rate, res_type='kaiser_fast')
+    background_data, background_rate = librosa.load(background_wav, sr=desired_sample_rate, res_type='kaiser_fast')
+
+    foreground_length = 1.0 * len(foreground_data.shape) / foreground_rate
+    background_length = 1.0 * len(background_data.shape) / background_rate
+
+    # Downsample both to 16 kHz - not necessary because of using librosa but keep just in case
+    # get_interp = lambda x, length: np.arange(0, length, 1.0 / x)
+    # foreground_data = np.array([np.interp(get_interp(desired_sample_rate, foreground_length),
+    #                             get_interp(foreground_rate, foreground_length),
+    #                             foreground_data[:,i]) for i in range(2)]).transpose()
+    # background_data = np.array([np.interp(get_interp(desired_sample_rate, background_length),
+    #                             get_interp(background_rate, background_length),
+    #                             background_data[:,i]) for i in range(2)]).transpose()
 
     # loop background if necessary
     if foreground_length > background_length:
@@ -69,15 +65,15 @@ def mix_wavs_raw(foreground_wav, background_wav, foreground_volume=0.6, backgrou
 
     # Generate output
     output = foreground_data * foreground_volume + \
-             background_data[:len(foreground_data), :] * background_volume
+             background_data[:len(foreground_data)] * background_volume
 
     return output
 
 def mix_wavs_to_file(foreground_wav, background_wav, output_wav_file,
-             foreground_volume=0.6, background_volume=0.4):
+             foreground_volume=0.6, background_volume=0.4, sample_rate=16000):
 
     data = mix_wavs_raw(foreground_wav, background_wav, foreground_volume, background_volume)
-    wavwrite(data, output_wav_file, desired_sample_rate)
+    librosa.output.write(output_wav_file, data, sample_rate)
 
 def random_wav(directory='wavfiles'):
     return directory + random.choice(os.listdir(directory))
