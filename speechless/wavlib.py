ed52b345309cf6b431d2e29aee9d82a604d62b97
Steffen Castle <steffen.castle@gmail.com>
2017-10-31 22:22:06 +0100

    Testing code

--- speechless/wavlib.py
+++ speechless/wavlib.py
@@ -5,6 +5,7 @@ import re
 import numpy as np
 import librosa
 import os, random
+import sys
 
 class WavTools:
     def __init__(self, directory):
@@ -14,6 +15,7 @@ class WavTools:
     def download_from_tag(tag='field-recording', directory='environmental', num_files=100):
         urls = WavTools.scrape_sound_urls(tag, num_files)
         WavTools.download_files(urls, directory)
+        WavTools.segment_wavs(directory)
 
     @staticmethod
     def scrape_sound_urls(tag='field-recording', num_files=100):
@@ -26,6 +28,19 @@ class WavTools:
             for match in regex.findall(page):
                 urls.append(match[0])
         return urls[:num_files]
+
+    @staticmethod
+    def segment_wavs(directory):
+        files = WavTools.absoluteFilePaths(directory)
+        for file in files:
+            wav_data, _ = librosa.load(str(file), sr=16000, res_type='kaiser_fast')
+            # segment into 10-second chunks
+            for i in range(0, len(wav_data), 160000):
+                stop_index = len(wav_data) if stop_index > len(wav_data) else i + 160000
+                data = wav_data[i:stop_index]
+                output_wav_file = os.path.splitext(file)[0] + str(int(i / 1000)) + '.wav'
+                librosa.output.write(output_wav_file, data, 16000)
+            os.remove(file)
 
     @staticmethod
     def download_files(urls, directory='wavfiles'):
@@ -46,6 +61,40 @@ class WavTools:
             response = s.get('http://freesound.org' + download_url)
             with open('{}/{}'.format(directory, filename), 'wb') as outfile:
                 outfile.write(response.content)
+
+    @staticmethod
+    def mix_wav(foreground_wav, foreground_volume=0.6, background_volume=0.4):
+        desired_sample_rate = 16000
+        background_data = np.array()
+
+        # load foreground
+        foreground_data, foreground_rate = librosa.load(str(foreground_wav),
+                                                        sr=desired_sample_rate, res_type='kaiser_fast')
+
+        # load enough background wavs to cover length of foreground
+        first = True
+
+        while len(background_data) < len(foreground_data):
+
+            background_wav = WavTools.get_next_wav()
+            wav_background_data, wav_background_rate = librosa.load(str(background_wav),
+                                                        sr=desired_sample_rate, res_type='kaiser_fast')
+            if first:
+                first = False
+                background_data = background_data[WavTools.curr_wav_start:]
+
+            background_data = np.concatenate(background_data, wav_background_data, axis=0)
+
+        # shorten background to be same length as foreground
+        WavTools.curr_wav_start = len(background_data) - len(foreground_data)
+        WavTools.curr_wav_idx -= 1
+        background_data = background_data[:len(foreground_data)]
+
+        # Generate output
+        output = foreground_data * foreground_volume + \
+                 background_data[:len(foreground_data)] * background_volume
+
+        return output
 
     @staticmethod
     def mix_wavs_raw(foreground_wav, background_wav, foreground_volume=0.6, background_volume=0.4):
@@ -80,6 +129,31 @@ class WavTools:
         return output
 
     @staticmethod
+    def absoluteFilePaths(directory):
+        for dirpath,_,filenames in os.walk(directory):
+            for f in filenames:
+                yield os.path.abspath(os.path.join(dirpath, f))
+
+    @staticmethod
+    def get_background_wav_list(wavdir):
+        WavTools.wavlist = WavTools.absoluteFilePaths(wavdir)
+
+    wavlist = []
+    curr_wav_idx = 0
+    curr_wav_start = 0
+
+    @staticmethod
+    def get_next_wav():
+        numwavs = len(WavTools.wavlist)
+        if numwavs > 0:
+            curr_wav = WavTools.wavlist[WavTools.curr_wav_idx]
+            WavTools.curr_wav_idx += 1
+            if numwavs <= WavTools.curr_wav_idx:
+                WavTools.curr_wav_idx = 0
+            return curr_wav
+        return ''
+
+    @staticmethod
     def mix_wavs_to_file(foreground_wav, background_wav, output_wav_file,
                  foreground_volume=0.6, background_volume=0.4, sample_rate=16000):
 
@@ -89,3 +163,7 @@ class WavTools:
     def random_wav(self, directory):
         wavdir = self.directory + '/' + directory
         return wavdir + '/' + random.choice(os.listdir(wavdir))
+
+if __name__ == "__main__":
+    directory = sys.argv[1]
+    WavTools.download_from_tag(directory='/data/speechless-data/wav/environmental')
