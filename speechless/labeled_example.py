885dd9dcebdfdeb265d0536092921f0fd38a589f
Steffen Castle <steffen.castle@gmail.com>
2017-11-06 18:16:17 +0100

    Testing code

--- speechless/labeled_example.py
+++ speechless/labeled_example.py
@@ -103,6 +103,10 @@ class LabeledExample(LabeledSpectrogram):
         return abs(self._complex_spectrogram())
 
     def _complex_spectrogram(self) -> ndarray:
+        print(self.id)
+        print(self.duration_in_s)
+        print(self.fourier_window_length)
+        print(self.hop_length)
         return librosa.stft(y=self.get_raw_audio(), n_fft=self.fourier_window_length, hop_length=self.hop_length)
 
     def mel_frequencies(self) -> List[float]:
