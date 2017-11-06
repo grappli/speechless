85270ce9db8437b7e4ffdc0cc16fc8447dca29c8
Steffen Castle <steffen.castle@gmail.com>
2017-11-06 18:59:14 +0100

    Testing code

--- speechless/configuration.py
+++ speechless/configuration.py
@@ -107,6 +107,7 @@ class Configuration:
         def tib_corpus(train_ratio: int = 0.9, include_training: bool = False):
             tibCorpus = Corpus.load(Path('/data/TIB_dataset/corpus.csv'), augment=False)
             train, test = TrainingTestSplit.randomly(train_ratio)(tibCorpus.examples)
+            print("Removing bad audio.")
             train = [example for example in train if example.duration_in_s > 0.0]
             test = [example for example in test if example.duration_in_s > 0.0]
             return Corpus(train if include_training else [], test)
